0080c5365a02ff1f0f9b5b3c85d0c8caad2c43de
Debabrata <auddya@wisc.edu>
2019-02-06 08:11:03 -0600

    Final Matlab

---
+++ iterationTest2.m
@@ -0,0 +1,41 @@
+close all
+clear all
+N = 101;
+L = 0.005; %0.005m
+g = 100000; %100000 Pa/m
+mu_zero = 1000; %1000 Pa
+nu = 1000; %1000 Pa
+K = 50000000000; %50GPa/m2
+T = 200; %200Pa
+w = zeros(N,1); %Extracellular displacement
+u = zeros(N,1); %Intracellular displacement
+x = zeros(N,1); %x position, useful when plotting
+delta = (2*L)/(N-1); %Spacing along x direction
+iterations = 100;
+for i = 1:N
+    x(i) = L*(2*(i-1)/(N-1)-1); 
+    mu(i) = mu_zero + g*x(i);
+end
+for k = 1:iterations
+    for i = 2:(N-1)
+        a(i) = 4*mu(i)*(w(i+1)+w(i-1))+(mu(i+1)-mu(i-1))*(w(i+1)-w(i-1));
+        b(i) = 4*nu*(u(i+1)+u(i-1));
+        A(i) = 8*mu(i) + K*delta*delta;
+        C = 8*nu + K*delta*delta;
+        B = K*delta*delta;
+        u(i) = (a(i)*B + A(i)*b(i))/(A(i)*C - B*B);
+        w(i) = (a(i)/A(i)) + (B/A(i))*((a(i)*B + A(i)*b(i))/(A(i)*C - B*B));
+    end
+    %Apply Boundary Conditions
+    u(1) = u(2) + (T*delta/(4*nu));
+    w(1) = w(2);
+    u(N) = u(N-1) - (T*delta/(4*nu));
+    w(N) = w(N-1);
+end 
+for i = 1:N
+    h(i) = u(i)-w(i);
+end 
+plot(x,w)  % if you want plot with x in mm, use ?plot(x*1000,h)?
+xlabel('x');
+ylabel('u - w');
+title('Difference between extracellular and intracellular displacements');
