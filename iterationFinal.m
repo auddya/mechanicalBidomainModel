62e9e69668206ff8670367a1ef01b0ae072ec0c4
Debabrata Auddya <auddya@wisc.edu>
2019-02-13 12:21:04 -0600

    Strain included now

--- iterationFinal.m
+++ iterationFinal.m
@@ -6,7 +6,7 @@ L = 0.0005; %0.005m
 g = 60000000; %100000 Pa/m
 mu_zero = 40000; %1000 Pa
 nu = mu_zero; %1000 Pa
-K = 2000000000000; %50GPa/m2
+K = 200000000000000; %50GPa/m2
 T = 200; %200Pa
 w = zeros(N,1); %Extracellular displacement
 u = zeros(N,1); %Intracellular displacement
@@ -17,7 +17,7 @@ for i = 1:N
     mu(i) = mu_zero + g*x(i);
 end
 overc = 0;
-resitol = 1e-16;
+resitol = 1e-12;
 for over = 1.00:0.01:1.99
   overc = overc+1;  
   residmax = 1000000000;
@@ -28,6 +28,7 @@ for over = 1.00:0.01:1.99
         a(i) = 4*mu(i)*(w(i+1)+w(i-1))+(mu(i+1)-mu(i-1))*(w(i+1)-w(i-1));
         b(i) = 4*nu*(u(i+1)+u(i-1));
         A(i) = 8*mu(i) + K*delta*delta;
+        exx(i) = (u(i+1)-u(i-1))/(2*delta);
         C = 8*nu + K*delta*delta;
         B = K*delta*delta;
         resi_u(i) = (a(i)*B + A(i)*b(i))/(A(i)*C - B*B) - u(i);
@@ -40,6 +41,8 @@ for over = 1.00:0.01:1.99
     w(1) = w(2);
     u(N) = u(N-1) - (T*delta/(4*nu));
     w(N) = w(N-1);
+    exx(1) = (u(2)-u(1))/(delta);
+    exx(N) = (u(N)-u(N-1))/(delta);
     if(kay>1)
       residmax = max(max(abs(resi_u)),max(abs(resi_w)));
     end
@@ -49,8 +52,10 @@ for over = 1.00:0.01:1.99
 %   toc
  ubackup = u;
  wbackup = w;
+ exxbackup = exx;
  u(1:end) = 0.0;
  w(1:end) = 0.0;
+ exx(1:end) = 0.0;
  kay_store(overc) = kay;
  residmax_store(overc) = residmax;
  end
@@ -59,17 +64,17 @@ for over = 1.00:0.01:1.99
  for i = 1:N
  h(i) = ubackup(i)-wbackup(i);
  end
- plot(x,h)
+ plot(x,exxbackup)
  %overc = 0;
  %hold on
  %legend('tolerance = 1e-16')
  %set(gca,'XTick',[0 10 20 30 40 50 60 70 80 90 100]);
  %set(gca,'XTickLabel',[1 1.09 1.19 1.29 1.39 1.49 1.59 1.69 1.79 1.89]);
  xlabel('Length (in microns)');
- ylabel('u-w(in microns)');
- title('u-w');
- xt = arrayfun(@num2str,get(gca,'xtick')*10^6,'un',0)
- yt = arrayfun(@num2str,get(gca,'ytick')*10^6,'un',0)
+ ylabel('\epsilon_{xx}');
+ title('Strain');
+ xt = arrayfun(@num2str,get(gca,'xtick')*10^6,'un',0);
+ yt = arrayfun(@num2str,get(gca,'ytick'),'un',0);
  set(gca,'xticklabel',xt,'yticklabel',yt)
  %xlim([-0.00002 0.00002]);
  %ylim([-0.0000003 0.0000003]);
